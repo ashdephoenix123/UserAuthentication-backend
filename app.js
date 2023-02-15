e91b04878f98598b818d1084d8d2de2209f141ba
akash <akashsarki12345@gmail.com>
2023-02-15 10:20:25 +0530

    localhost added url

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ require('./db/conn')
 const app = express();
 app.use(cookieParser()) //handles the cookies
 app.use(express.json())  // tells the server to serve the json responses in object form
-app.use(cors())
-// app.use(cors({
-//     origin: "https://userauthenticationreact.netlify.app",
-//     credentials: true
-// }))
+// app.use(cors())
+app.use(cors({
+    origin: ["http://localhost/5000"],
+    credentials: true
+}))
 app.use(require('./router/auth'));
 
 const PORT = process.env.PORT || 5000;
